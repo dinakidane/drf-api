340bc7ecf3d5a803379dd53e9d6ba8dd105bbc94
dinakidane <dinakidane@hotmail.com>
2024-04-09 15:45:02 +0000

    urls

---
+++ comments/views.py
@@ -0,0 +1,20 @@
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
+
+class CommentList (generics.ListCreateAPIView):
+    serializers_class = CommentSerializer
+    permission_class = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
+
+
+
