f401cd5def097b2380baa30fe5be7a67d9f539df
dinakidane <dinakidane@hotmail.com>
2024-04-11 18:34:14 +0000

    deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -125,14 +125,21 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.2/ref/settings/#database
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+    print('connected')
+
 
 
 # Password validation
