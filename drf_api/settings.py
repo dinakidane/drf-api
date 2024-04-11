28cbd37c9c04525d74b92df1b5d2dc9f89c5a7b0
dinakidane <dinakidane@hotmail.com>
2024-04-11 22:07:29 +0000

    heroku

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '<drf-api>.herokuapp.com', '8000-dinakidane-drfapi-gh0vl7vhzzd.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['localhost', '<drf-api>.herokuapp.com']
 
 
 # Application definition
