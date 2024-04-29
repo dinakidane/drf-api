38f2ab68be1f135ed434cf65c8c750db6bbd2568
dinakidane <dinakidane@hotmail.com>
2024-04-29 17:06:04 +0000

    api

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -115,14 +115,16 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
+CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+    ]
+
 #if 'CLIENT_ORIGIN' in os.environ:
     #CORS_ALLOWED_ORIGINS = [
-        ##os.environ.get('CLIENT_ORIGIN')
+        #os.environ.get('CLIENT_ORIGIN')
     #]
 #else:
-    #CORS_ALLOWED_ORIGIN_REGEXES = [
-        #r"^https://.*\.gitpod\.io$",
-    #]
+
 
 CORS_ALLOW_CREDENTIALS = True
 
