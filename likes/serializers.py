a006ed047ee9cec04f333adceea79ae0d960eab8
dinakidane <dinakidane@hotmail.com>
2024-05-03 17:15:38 +0000

    fix likes

--- likes/serializers.py
+++ likes/serializers.py
@@ -2,7 +2,12 @@ from django.db import IntegrityError
 from rest_framework import serializers
 from likes.models import Like
 
+
 class LikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Like model
+    The create method handles the unique constraint on 'owner' and 'post'
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
@@ -13,6 +18,6 @@ class LikeSerializer(serializers.ModelSerializer):
         try:
             return super().create(validated_data)
         except IntegrityError:
-            raise serializers.Validation({
+            raise serializers.ValidationError({
                 'detail': 'possible duplicate'
             })
