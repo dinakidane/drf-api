58d69347d67856c48e296954fd89e072b16047e1
dinakidane <dinakidane@hotmail.com>
2024-05-03 17:19:44 +0000

    fix views

--- likes/views.py
+++ likes/views.py
@@ -3,15 +3,23 @@ from drf_api.permissions import IsOwnerOrReadOnly
 from likes.models import Like
 from likes.serializers import LikeSerializer
 
+
 class LikeList(generics.ListCreateAPIView):
-    serializers_class = LikeSerializer
-    permission_class = [permissions.IsAuthenticatedOrReadOnly]
+    """
+    List likes or create a like if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
     queryset = Like.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-class LikeDetail(generics.RetrieveUpdateDestroyAPIView):
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like or delete it by id if you own it.
+    """
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
