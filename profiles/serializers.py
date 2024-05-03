e2463903252d25dd29e7af4878d2604bfe3929bc
dinakidane <dinakidane@hotmail.com>
2024-05-03 17:01:49 +0000

    fix profiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,22 +2,22 @@ from rest_framework import serializers
 from .models import Profile
 from followers.models import Follower
 
+
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
     posts_count = serializers.ReadOnlyField()
-    followed_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
-
-    def get_following_id(self,obj):
+    def get_following_id(self, obj):
         user = self.context['request'].user
-        if user.is_autheniticated:
+        if user.is_authenticated:
             following = Follower.objects.filter(
                 owner=user, followed=obj.owner
             ).first()
@@ -29,6 +29,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'image', 'is_owner', 'post_count', 
-            'following_count', 'followed_count'
+            'content', 'image', 'is_owner', 'following_id',
+            'posts_count', 'followers_count', 'following_count',
         ]
